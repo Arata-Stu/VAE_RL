00cec9eb99bdd1bbeedb1e0d684d5adf28db2d51
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-03-01 20:30:19 +0900

    update

--- train_vae.py
+++ train_vae.py
@@ -61,6 +61,8 @@ def main(config: DictConfig):
 
     
     model = VAETrainer(latent_dim=config.latent_dim, learning_rate=config.learning_rate)
+    if ckpt_path is not None:
+        model = model.load_from_checkpoint(config.ckpt_path)
     
     # チェックポイントの設定 (Top 3 の val_loss のモデルを保存)
     checkpoint_callback = ModelCheckpoint(
